04e02935f612df8bda5d57d216b593aa7d1b85ea
Dan Moore <dan@makeitdoathing.com>
2015-04-06 14:56:46 -0400

    whoops

--- ofxVideoRecorderExample/src/testApp.cpp
+++ ofxVideoRecorderExample/src/testApp.cpp
@@ -4,27 +4,27 @@
 void testApp::setup(){
     sampleRate = 44100;
     channels = 2;
-
+    
     ofSetFrameRate(60);
     ofSetLogLevel(OF_LOG_VERBOSE);
     vidGrabber.setDesiredFrameRate(30);
     vidGrabber.initGrabber(640, 480);
-//    vidRecorder.setFfmpegLocation(ofFilePath::getAbsolutePath("ffmpeg")); // use this is you have ffmpeg installed in your data folder
-
+    //    vidRecorder.setFfmpegLocation(ofFilePath::getAbsolutePath("ffmpeg")); // use this is you have ffmpeg installed in your data folder
+    
     fileName = "testMovie";
     fileExt = ".mov"; // ffmpeg uses the extension to determine the container type. run 'ffmpeg -formats' to see supported formats
-
+    
     // override the default codecs if you like
     // run 'ffmpeg -codecs' to find out what your implementation supports (or -formats on some older versions)
-    vidRecorder.setVideoCodec("mpeg4"); 
+    vidRecorder.setVideoCodec("mpeg4");
     vidRecorder.setVideoBitrate("800k");
     vidRecorder.setAudioCodec("mp3");
     vidRecorder.setAudioBitrate("192k");
-
-//    soundStream.listDevices();
-//    soundStream.setDeviceID(11);
+    
+    //    soundStream.listDevices();
+    //    soundStream.setDeviceID(11);
     soundStream.setup(this, 0, channels, sampleRate, 256, 4);
-
+    
     ofSetWindowShape(vidGrabber.getWidth(), vidGrabber.getHeight()	);
     bRecording = false;
     ofEnableAlphaBlending();
@@ -46,22 +46,22 @@ void testApp::update(){
 void testApp::draw(){
     ofSetColor(255, 255, 255);
     vidGrabber.draw(0,0);
-
+    
     stringstream ss;
     ss << "video queue size: " << vidRecorder.getVideoQueueSize() << endl
     << "audio queue size: " << vidRecorder.getAudioQueueSize() << endl
     << "FPS: " << ofGetFrameRate() << endl
     << (bRecording?"pause":"start") << " recording: r" << endl
     << (bRecording?"close current video file: c":"") << endl;
-
+    
     ofSetColor(0,0,0,100);
     ofRect(0, 0, 260, 75);
     ofSetColor(255, 255, 255);
     ofDrawBitmapString(ss.str(),15,15);
-
+    
     if(bRecording){
-    ofSetColor(255, 0, 0);
-    ofCircle(ofGetWidth() - 20, 20, 5);
+        ofSetColor(255, 0, 0);
+        ofCircle(ofGetWidth() - 20, 20, 5);
     }
 }
 
@@ -76,14 +76,12 @@ void testApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
     if(key=='r'){
         bRecording = !bRecording;
         if(bRecording && !vidRecorder.isInitialized()) {
-            vidRecorder.setup(fileName+ofGetTimestampString()+fileExt, vidGrabber.getWidth(), vidGrabber.getHeight(), 30, sampleRate, channels);
-//          vidRecorder.setup(fileName+ofGetTimestampString()+fileExt, vidGrabber.getWidth(), vidGrabber.getHeight(), 30); // no audio
-//            vidRecorder.setup(fileName+ofGetTimestampString()+fileExt, 0,0,0, sampleRate, channels); // no video
-//          vidRecorder.setupCustomOutput(vidGrabber.getWidth(), vidGrabber.getHeight(), 30, sampleRate, channels, "-vcodec mpeg4 -b 1600k -acodec mp2 -ab 128k -f mpegts udp://localhost:1234"); // for custom ffmpeg output string (streaming, etc)
+            
+            vidRecorder.setupCustomOutput(vidGrabber.getWidth(), vidGrabber.getHeight(), 30, sampleRate, channels, "-vcodec libx264 -pix_fmt yuv420p -b 1600k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://a.rtmp.youtube.com/live2/FROM.YOU.TUBE");
         }
     }
     if(key=='c'){
@@ -94,35 +92,35 @@ void testApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){
-
+    
 }
